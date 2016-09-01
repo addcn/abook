0ddd187fdbb3cc1ff9bd0a4a0686632175425efe
dodo <lhuibo@gmail.com>
2016-09-01 21:16:35 +0800

    蛛网评分控件

--- app/src/main/java/com/dodo/android/abook/features/graphics/DrawTextFragment.java
+++ app/src/main/java/com/dodo/android/abook/features/graphics/DrawTextFragment.java
@@ -7,9 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dodo.android.abook.R;
 import com.dodo.android.abook.widget.MyPathMeasure;
+import com.dodo.android.abook.widget.spider.CircularLayout;
+import com.dodo.android.abook.widget.spider.SpiderWebScoreView;
 
 
 public class DrawTextFragment extends Fragment {
@@ -70,4 +73,54 @@ public class DrawTextFragment extends Fragment {
         //myView = (MyPathMeasure) content.findViewById(R.id.myView);
         mHandler.sendEmptyMessage(START_ANIM);
     }
+
+    protected void initSpider() {
+
+
+        SpiderWebScoreView spiderWebScoreView1 = (SpiderWebScoreView) content.findViewById(R.id.spiderWeb_mainActivity_1);
+        CircularLayout circularLayout1 = (CircularLayout) content.findViewById(R.id.layout_mainActivity_circular1);
+
+        Score[] scores = new Score[]{
+                new Score(7.0f, R.mipmap.ic_launcher),
+                new Score(8.0f, R.mipmap.ic_launcher),
+                new Score(5.0f, R.mipmap.ic_launcher),
+                new Score(5.0f, R.mipmap.ic_launcher),
+                new Score(8.0f, R.mipmap.ic_launcher),
+                new Score(7.0f, R.mipmap.ic_launcher),
+        };
+        setup(spiderWebScoreView1, circularLayout1, scores);
+    }
+
+    private void setup(SpiderWebScoreView spiderWebScoreView, CircularLayout circularLayout, Score... scores){
+        float[] scoreArray = new float[scores.length];
+        for(int w = 0; w < scores.length; w++){
+            scoreArray[w] = scores[w].score;
+        }
+        spiderWebScoreView.setScores(10f, scoreArray);
+
+        circularLayout.removeAllViews();
+        for(Score score : scores){
+            TextView scoreTextView = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.score, circularLayout, false);
+            scoreTextView.setText(score.score+"");
+            if(score.iconId != 0){
+                scoreTextView.setCompoundDrawablesWithIntrinsicBounds(0, 0, score.iconId, 0);
+            }
+            circularLayout.addView(scoreTextView);
+        }
+    }
+
+    public static class Score{
+        public float score;
+        public int iconId;
+
+        public Score(float score, int iconId) {
+            this.score = score;
+            this.iconId = iconId;
+        }
+
+        public Score(float score) {
+            this.score = score;
+        }
+    }
+
 }
